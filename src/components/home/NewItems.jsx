40c14979527a6edbe573bf30c9bba27dff2ffb66
Jackson <z5397791@ad.unsw.edu.au>
2023-01-14 16:53:17 +1100

    Add link to custom author and nft pages in react links

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -47,7 +47,7 @@ const NewItems = () => {
             </div>
           </div>
           <OwlCarousel
-            className="owl-theme owl-custom"
+            className="owl-theme"
             loop
             margin={10}
             nav
@@ -57,7 +57,7 @@ const NewItems = () => {
               <div className="nft__item" key={index}>
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
+                    to={`/author/${item.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
                     title={item.title}
@@ -87,7 +87,7 @@ const NewItems = () => {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <img
                       src={item.nftImage}
                       className="lazy nft__item_preview"
@@ -96,7 +96,7 @@ const NewItems = () => {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <h4>{item.title}</h4>
                   </Link>
                   <div className="nft__item_price">{item.price} ETH</div>
