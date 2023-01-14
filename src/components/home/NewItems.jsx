df26900733f5aff3fdf914980da906b8c18b2c97
Jackson <z5397791@ad.unsw.edu.au>
2023-01-14 16:42:11 +1100

    Add owl carousel custom options

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -21,6 +21,21 @@ const NewItems = () => {
     fetchItems();
   }, []);
 
+  const options = {
+    items: 4,
+    margin: 10,
+    nav: true,
+    loop: true,
+    dots: false,
+    responsiveRefreshRate: 50,
+    responsive: {
+      1200: { items: 4 },
+      768: { items: 3 },
+      470: { items: 2 },
+      0: { items: 1 },
+    },
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -31,7 +46,13 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <OwlCarousel className="owl-theme owl-custom" loop margin={10} nav>
+          <OwlCarousel
+            className="owl-theme owl-custom"
+            loop
+            margin={10}
+            nav
+            {...options}
+          >
             {items.map((item, index) => (
               <div className="nft__item" key={index}>
                 <div className="author_list_pp">
