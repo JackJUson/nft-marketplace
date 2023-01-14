483b27220f3fcc439a461d7563b514849f58bede
Jackson <z5397791@ad.unsw.edu.au>
2023-01-14 18:57:53 +1100

    Fetch top sellers with axios in fetchSellers function

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,24 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+
+  async function fetchSellers() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setSellers();
+  }
+
+  useEffect(() => {
+    fetchSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
