c1ef2b0097f9372ec8a9b9dbbf12197396aa2a57
Jackson <z5397791@ad.unsw.edu.au>
2023-01-14 17:22:40 +1100

    Add seconds counter from expiry date of item

--- src/components/UI/Counter.jsx
+++ src/components/UI/Counter.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 export default function Counter({ item }) {
-  return (
-    <div className="de_countdown">5h 30m 32s!!</div>
-  )
+  const [time, setTime] = useState(Date.now());
+
+  const [second, setSecond] = useState();
+
+  function displayCounter() {
+    const timeLeftinMs = item.expiryDate - time;
+    setSecond(Math.floor(timeLeftinMs / 1000) % 60);
+  }
+
+  useEffect(() => {
+    displayCounter();
+    setTimeout(() => {
+      setTime(Date.now());
+    }, 1000);
+  }, [time]);
+
+  return <div className="de_countdown">{`${second}`}</div>;
 }
